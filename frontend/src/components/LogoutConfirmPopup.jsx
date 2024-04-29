6815c389344858e402ca55275e4a352529502901
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-04-29 15:41:46 +0530

    Updated LogoutConfirmPopup

--- frontend/src/components/LogoutConfirmPopup.jsx
+++ frontend/src/components/LogoutConfirmPopup.jsx
@@ -5,7 +5,7 @@ const LogoutConfirmPopup = ({ confirmLogout, cancelLogout }) => {
         <div className='fixed top-20 left-0 h-full w-full bg-black bg-opacity-50 flex justify-center items-center p-5 md:p-0 z-10 backdrop-blur-md'>
             <div className='w-full md:w-[500px] bg-white p-5 rounded-lg shadow text-center'>
                 <p>Are yu sure you want to logout?</p>
-                <div className='w-full flex justify-around items-center mt-5 text-sm'>
+                <div className='w-full flex justify-evenly items-center mt-5 text-sm'>
                     <button onClick={confirmLogout} className='bg-[#a87c7c] w-16 p-2 rounded-md shadow-md'>Yes</button>
                     <button onClick={cancelLogout} className='bg-[#a87c7c] w-16 p-2 rounded-md shadow-md'>No</button>
                 </div>                
